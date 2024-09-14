3d5f6852ca63d4db9759af5beba2fd368981b8b6
Shin-sibainu <shincode0712@gmail.com>
2024-09-14 10:58:55 +0900

    syntax highliter

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center h-[40rem]">
+      まだこのページは作れてないよ💦
+    </div>
+  );
+};
+
+export default NotFound;
--- src/components/common/StyledContent.tsx
+++ src/components/common/StyledContent.tsx
@@ -1,35 +1,87 @@
+"use client";
+
+import { CheckIcon, ClipboardIcon } from "@heroicons/react/16/solid";
+
 import {
   HTMLReactParserOptions,
   Element,
   domToReact,
   DOMNode,
 } from "html-react-parser";
+
 import parse from "html-react-parser";
+import { useState } from "react";
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 interface StyledContentProps {
   content: string;
 }
 
 const StyledContent = ({ content }: StyledContentProps) => {
+  const CodeBlock = ({
+    code,
+    language,
+  }: {
+    code: string;
+    language: string;
+  }) => {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+      navigator.clipboard.writeText(code);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 200);
+    };
+
+    return (
+      <div className="relative">
+        <button
+          onClick={handleCopy}
+          className="absolute top-2 right-2 p-2 bg-gray-700 rounded-md text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
+          {copied ? (
+            <CheckIcon className="h-5 w-5" />
+          ) : (
+            <ClipboardIcon className="w-5 h-5" />
+          )}
+        </button>
+
+        <SyntaxHighlighter
+          language={language}
+          style={vscDarkPlus}
+          customStyle={{
+            margin: 0,
+            padding: "1rem",
+            borderRadius: "0.5rem",
+          }}
+        >
+          {code}
+        </SyntaxHighlighter>
+      </div>
+    );
+  };
+
   const options: HTMLReactParserOptions = {
     replace: (domNode) => {
       if (domNode instanceof Element) {
         switch (domNode.name) {
           case "h1":
             return (
-              <h1 className="text-4xl font-bold my-4">
+              <h1 className="text-4xl font-bold my-8">
                 {domToReact(domNode.children as DOMNode[], options)}
               </h1>
             );
           case "h2":
             return (
-              <h2 className="text-3xl font-semibold my-3">
+              <h2 className="text-3xl font-semibold my-8">
                 {domToReact(domNode.children as DOMNode[], options)}
               </h2>
             );
           case "h3":
             return (
-              <h3 className="text-2xl font-medium my-2">
+              <h3 className="text-2xl font-medium my-4">
                 {domToReact(domNode.children as DOMNode[], options)}
               </h3>
             );
@@ -53,7 +105,7 @@ const StyledContent = ({ content }: StyledContentProps) => {
             );
           case "li":
             return (
-              <li className="my-1">
+              <li className="my-4">
                 {domToReact(domNode.children as DOMNode[], options)}
               </li>
             );
@@ -73,33 +125,27 @@ const StyledContent = ({ content }: StyledContentProps) => {
               </blockquote>
             );
           case "code":
-            // インライン・コードのスタイリング
-            if (domNode.parent?.name !== "pre") {
+            if ((domNode.parent as Element)?.name !== "pre") {
               return (
-                <code className="bg-gray-300 text-red-600 rounded px-1 py-0.5 font-mono text-sm">
+                <code className="bg-gray-100 text-red-500 rounded px-1 py-0.5 font-mono text-sm">
                   {domToReact(domNode.children as DOMNode[], options)}
                 </code>
               );
             }
-            // コードブロックのスタイリング
-            return (
-              <code className="block bg-gray-800 text-gray-100 rounded-lg p-4 font-mono text-sm overflow-x-auto">
-                {domToReact(domNode.children as DOMNode[], options)}
-              </code>
-            );
+            const language =
+              domNode.attribs.class?.replace("language-", "") || "plaintext";
+            const code = (domNode.children[0] as unknown as { data: string })
+              .data;
+            return <CodeBlock code={code} language={language} />;
           case "pre":
-            return (
-              <pre className="my-4">
-                {domToReact(domNode.children as DOMNode[], options)}
-              </pre>
-            );
+            return domToReact(domNode.children as DOMNode[], options);
           // 他の要素に対するスタイリングをここに追加
         }
       }
     },
   };
 
-  return <div className="prose prose-md">{parse(content, options)}</div>;
+  return <div className="max-w-none">{parse(content, options)}</div>;
 };
 
 export default StyledContent;
